94c22f05926fe597103a4a96d2e28482afb00753
EdgarHE <mingcheng.he@hotmail.com>
2018-04-18 13:15:02 +0000

    Update curtain.py

--- curtain.py
+++ curtain.py
@@ -2,15 +2,21 @@ import time
 import sys
 import socket
 import thread
+import threading
 
 temp = 0
-joystatus = -1
+joystatus = 0
+
+bar_length=50
 
 def TCP(sock, addr): 
 	while True:
 		global joystatus
 		data = sock.recv(1024) 
-		time.sleep(1) 
+		time.sleep(0.1)
+		if not data: 
+			break
+		#print data
 		joystatus = int(data)
 
 	sock.close() 
@@ -20,32 +26,48 @@ def server_thread(HOST, PORT):
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.bind((HOST, PORT))
 	s.listen(1) 
-	print('\nServer is running...\n')
+	#print('\nServer is running...\n')
 	sock, addr = s.accept()
-	print('Accept new connection from %s.' %addr[0])
+	#print('Accept new connection from %s.' %addr[0])
 	while True:
 		sock, addr = s.accept()
 		TCP(sock, addr)
 		
 		
-def progress_test(x):
-    bar_length=50
+def progress_right(x):
     global temp
-    for percent in range(temp, x):
-    	hhh = 100-percent
-        hashes = '=' * int((100.0-percent)/100.0 * bar_length)
+    while temp < x:
+        temp = temp + 1
+        hashes = '=' * int((temp)/100.0 * bar_length)
         spaces = ' ' * (bar_length - len(hashes))
-        sys.stdout.write("\rPercent: [%s] %d%% "%(hashes + spaces, hhh))
+        sys.stdout.write("\rPercent: [%s] %d%% "%(hashes + spaces, temp))
         sys.stdout.flush()
-        temp = x
-        time.sleep(0.1)
+        time.sleep(0.01)
+        
+    
+def progress_left(x):
+    global temp
+    while temp > x:
+        temp = temp - 1
+        hashes = '=' * int((temp)/100.0 * bar_length)
+        spaces = ' ' * (bar_length - len(hashes))
+        sys.stdout.write("\rPercent: [%s] %d%% "%(hashes + spaces, temp))
+        sys.stdout.flush()
+        
+        time.sleep(0.01)
+    #temp = x
 
 def runcurtain():
 	try:
-		progress_test(1)
+                hashes = '=' * int((temp)/100.0 * bar_length)
+                spaces = ' ' * (bar_length - len(hashes))
+                sys.stdout.write("\rPercent: [%s] %d%% "%(hashes + spaces, temp))
+                sys.stdout.flush()
 		while True:
-			if joystatus == 1:
-				progress_test(temp+2)
+			if joystatus == 1 and temp > 1:
+				progress_left(temp-2)
+			elif joystatus == 2 and temp < 100:
+				progress_right(temp+2)
 	except KeyboardInterrupt, e: 
 		pass
 
